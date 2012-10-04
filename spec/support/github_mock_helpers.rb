52195bc78962533138ff0976675c0b894a410b3f
Jason Morrison <jason.p.morrison@gmail.com>
2012-10-04 17:12:18 +0700

    Refactor GithubMockHelpers to extract #assert_options

--- spec/support/github_mock_helpers.rb
+++ spec/support/github_mock_helpers.rb
@@ -13,34 +13,33 @@ module GithubMockHelpers
   end
 
   def mock_github_user_repos(options={})
-    raise "must include :oauth_token" unless options[:oauth_token]
-    raise "must include :repos" unless options[:repos]
+    assert_options(options, :oauth_token, :repos)
 
     json_response = options[:repos].to_json
     stub_request(:get, "https://api.github.com/user/repos?access_token=#{options[:oauth_token]}&per_page=1000").to_return(status: 200, body: json_response)
   end
 
   def mock_github_repo_hook(options={})
-    raise "must include :oauth_token" unless options[:oauth_token]
-    raise "must include :user_name" unless options[:user_name]
-    raise "must include :repo_name" unless options[:repo_name]
-    raise "must include :resulting_hook_id" unless options[:resulting_hook_id]
+    assert_options(options, :oauth_token, :user_name, :repo_name, :resulting_hook_id)
 
     json_response = { url: 'http://something', id: options[:resulting_hook_id] }.to_json
     stub_request(:post, "https://api.github.com/repos/#{options[:user_name]}/#{options[:repo_name]}/hooks?access_token=#{options[:oauth_token]}").
       to_return(status: 201, body: json_response)
   end
 
-  # TODO: Refactor to extract `raise "must include :foo" unless options[:foo]`
   def mock_github_set_commit_status(options={})
-    raise "must include :oauth_token" unless options[:oauth_token]
-    raise "must include :user_name" unless options[:user_name]
-    raise "must include :repo_name" unless options[:repo_name]
-    raise "must include :sha" unless options[:sha]
+    assert_options(options, :oauth_token, :user_name, :repo_name, :sha)
 
     json_response = options[:json_response] || { whatever: 'yeah' }.to_json
-
     url = "https://api.github.com/repos/#{options[:user_name]}/#{options[:repo_name]}/statuses/#{options[:sha]}?access_token=#{options[:oauth_token]}"
     stub_request(:post, url).to_return(status: 201, body: json_response)
+  end
+
+  private
+
+  def assert_options(options, *required_keys)
+    required_keys.each do |key|
+      raise "must include :#{key}" unless options[key]
+    end
   end
 end
