c24bb1120670a5a129d2dd53256b334ef28756e1
Jason Morrison <jason.p.morrison@gmail.com>
2012-10-06 16:37:46 +0700

    Do not prompt user to agree twice

--- spec/acceptance/sign_agreement_spec.rb
+++ spec/acceptance/sign_agreement_spec.rb
@@ -26,5 +26,18 @@ feature "Agreeing to a CLA" do
     page.should have_content('You have agreed to the CLA for the_owner/the_project.')
   end
 
-  scenario "Do not allow me to agree twice"
+  scenario "Do not allow me to agree twice" do
+    visit '/agreements/the_owner/the_project'
+    click_link 'Sign in with GitHub to agree to this CLA'
+
+    page.should have_content('The CLA text')
+    page.should have_no_content('Sign in with GitHub')
+    page.should have_content('I agree')
+
+    click_link 'I agree'
+    page.should have_content('You have agreed to the CLA for the_owner/the_project.')
+    page.should have_no_content('I agree')
+  end
+
+  scenario "Signing the agreement updates commit statuses for pull requests I've issued"
 end
