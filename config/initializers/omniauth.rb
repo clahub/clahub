7b317bc898297e1dfa2fb6b8d4327445547e602f
Jason Morrison <jason.p.morrison@gmail.com>
2012-10-07 18:19:31 +0700

    Ensure GitHub oauth keys on boot

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+raise "Provide ENV['GITHUB_KEY'] and ENV['GITHUB_SECRET']" unless Rails.env.test? || (ENV['GITHUB_KEY'] && ENV['GITHUB_SECRET'])
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "repo:status,public_repo"
