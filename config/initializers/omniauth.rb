71b9112b4a2123ba43462d8484e973986741618c
Jason Morrison <jason.p.morrison@gmail.com>
2012-10-08 15:12:31 +0700

    Go easy on the envs, heroku prep steps lack ENV vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
-raise "Provide ENV['GITHUB_KEY'] and ENV['GITHUB_SECRET']" unless Rails.env.test? || (ENV['GITHUB_KEY'] && ENV['GITHUB_SECRET'])
+if Rails.env.development?
+  if ENV['GITHUB_KEY'].blank? || ENV['GITHUB_SECRET'].blank?
+    raise "Provide ENV['GITHUB_KEY'] and ENV['GITHUB_SECRET']"
+  end
+end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
