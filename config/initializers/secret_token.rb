96c0fb21aae4d5717b49291f113ea37ad3cc52a9
Jason Morrison <jason.p.morrison@gmail.com>
2013-01-09 15:31:28 -0800

    Extract ENV['SECRET_TOKEN'] for production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Clahub::Application.config.secret_token = 'c73131d9253abeaf053a8ff895055c7be924a86048cfc5a40290613e3b67a0e920150061f1bcfee5a568aa3ee5b89c571aa70e50beda0612c59551643a2ef029'
+Clahub::Application.config.secret_token = if Rails.env.production?
+  ENV['SECRET_TOKEN'] || raise("Set ENV['SECRET_TOKEN'] for production")
+else
+  'c73131d9253abeaf053a8ff895055c7be924a86048cfc5a40290613e3b67a0e920150061f1bcfee5a568aa3ee5b89c571aa70e50beda0612c59551643a2ef029'
+end
+
