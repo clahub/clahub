4d4e649b3d8c38590893f1e94076ea6f42328a6b
Jason Morrison <jason.p.morrison@gmail.com>
2013-01-27 17:58:20 -0800

    Link to Privacy Policy and Terms of Service

--- spec/acceptance/visitor_pages_spec.rb
+++ spec/acceptance/visitor_pages_spec.rb
@@ -15,3 +15,19 @@ feature "The 'Why CLAs?' page" do
     page.should have_content('What is a CLA')
   end
 end
+
+feature "privacy policy page" do
+  scenario 'Explains privacy policy to a visitor' do
+    visit '/'
+    click_link 'Privacy Policy'
+    page.should have_content('you must read through the entire Privacy Policy below')
+  end
+end
+
+feature "terms page" do
+  scenario 'Explains terms to a visitor' do
+    visit '/'
+    click_link 'Terms of Service'
+    page.should have_content('sets forth the conditions and terms under which users may access')
+  end
+end
