8dac9f9bb9aa002c29a03f0064ff2805adeb474c
Esteve Fernandez <esteve@osrfoundation.org>
2014-10-29 15:32:59 -0700

    Add GitHubLimited strategy to avoid using public_repo scope on GitHub

---
+++ app/controllers/github_limited_oauth_controller.rb
@@ -0,0 +1,2 @@
+class GithubLimitedOauthController < GithubOauthController
+end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,23 @@
+OmniAuth.config.add_camelization 'github_limited', 'GitHubLimited'
+
+module OmniAuth
+  module Strategies
+    class GitHubLimited < GitHub
+      def name
+        "github_limited"
+      end
+    end
+  end
+end
+
 if Rails.env.development?
-  if ENV['GITHUB_KEY'].blank? || ENV['GITHUB_SECRET'].blank?
-    raise "Provide ENV['GITHUB_KEY'] and ENV['GITHUB_SECRET']"
+  if ENV['GITHUB_KEY'].blank? || ENV['GITHUB_SECRET'].blank? || ENV['GITHUB_LIMITED_KEY'].blank? || ENV['GITHUB_LIMITED_SECRET'].blank?
+    raise "Provide ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], ENV['GITHUB_LIMITED_KEY'] and ENV['GITHUB_LIMITED_SECRET']"
   end
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "public_repo"
+  provider :github_limited, ENV['GITHUB_LIMITED_KEY'], ENV['GITHUB_LIMITED_SECRET'], scope: "(no scope)"
 end
