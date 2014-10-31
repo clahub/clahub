2644d32b779aba97ce538697814eea2cbd49a7a8
José Dulanto <jose.dulanto@live.com>
2014-10-31 15:00:25 -0600

    fixes remaining tests, all green now

--- spec/factories/agreement.rb
+++ spec/factories/agreement.rb
@@ -3,14 +3,6 @@ FactoryGirl.define do
     association :user
     text "Generic CLA text"
     github_repositories ["some_user/some_repo"]
-    
-    after(:create) do |agreement|
-      # puts "================ #{agreement.inspect}"
-      agreement.github_repositories.each do |repo|
-        r = repo.split('/')
-        agreement.repositories << create(:repository, user_name: r.first, repo_name: r.last)
-      end
-    end
   end
   
 end
